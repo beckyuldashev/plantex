5ac2f56aaf008404a6fa4aa98223b82b8e0504f3
Beck Yuldashev <yuldashev.bek@gmail.com>
2021-11-14 11:04:53 +0300

    Added mediaqueries and animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,3 +145,21 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme());
     localStorage.setItem('selected-icon', getCurrentIcon());
 });
+
+
+// =============== SCROLLREAVEAL ANIMATION ===============
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true
+});
+
+
+sr.reveal(`.home__data`);
+sr.reveal(`.home__img`, {delay: 500});
+sr.reveal(`.home__social`, {delay: 600});
+sr.reveal(`.about__img, .contact__box`, {origin: 'left'});
+sr.reveal(`.about__data, .contact__form`, {origin: 'right'});
+sr.reveal(`.steps__card, .product__card, .questions__group, .footer__container`, {interval: 100});
